b9c0c7bef2a0be95f285c82d119a3522cfabb27b
John Doe <john.doe@example.com>
2025-03-18 21:25:57 +0700

    uhyuyh

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const scene = new THREE.Scene()
 const loader = new THREE.TextureLoader()
 
-// Создаем сферу для фона
+// Фон
 const backgroundGeometry = new THREE.SphereGeometry(50, 32, 32)
 const backgroundTexture = loader.load('/hrush.webp')
 backgroundTexture.mapping = THREE.EquirectangularReflectionMapping
@@ -21,51 +21,60 @@ function animateBackground() {
   backgroundMesh.rotation.y = backgroundRotation
 }
 
+// Камера
 const camera = new THREE.PerspectiveCamera(
-  100, // Угол обзора увеличен для эффекта рыбьего глаза
+  90,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
 camera.position.set(0, 0, 2.9)
 
+// Рендерер
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
+renderer.physicallyCorrectLights = true
 document.body.appendChild(renderer.domElement)
 
-const light = new THREE.PointLight(0xffffff, 2, 15)
+// Освещение
+const light = new THREE.DirectionalLight(0xffffff, 1.5)
 light.position.set(-1, 3, 5)
 light.castShadow = true
 scene.add(light)
 
+const ambientLight = new THREE.AmbientLight(0x404040, 1.5)
+scene.add(ambientLight)
+
+// Загрузка текстуры для пирамиды
 const moneyTexture = loader.load('/money.png', (texture) => {
   texture.wrapS = THREE.RepeatWrapping
   texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(8, 8)
+  texture.repeat.set(4, 2) // Настроил повторение текстуры
 })
+
+// Пирамида
 const pyramidGeometry = new THREE.ConeGeometry(2, 3, 4)
-const pyramidMaterial = new THREE.MeshStandardMaterial({
+const pyramidMaterial = new THREE.MeshPhongMaterial({
   map: moneyTexture,
 })
 const pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial)
 pyramid.castShadow = true
 scene.add(pyramid)
 
+// Пол для теней
 const groundGeometry = new THREE.PlaneGeometry(10, 10)
-const groundMaterial = new THREE.ShadowMaterial({
-  opacity: 1,
-})
+const groundMaterial = new THREE.ShadowMaterial({ opacity: 0.5 })
 const ground = new THREE.Mesh(groundGeometry, groundMaterial)
 ground.rotation.x = -Math.PI / 3
 ground.position.y = -2.8
 ground.receiveShadow = true
 scene.add(ground)
 
-let baseRotationX = 0.5
-let baseRotationZ = 0.3
-pyramid.rotation.set(baseRotationX, 0, baseRotationZ)
+// Вращение пирамиды
+pyramid.rotation.set(0.5, 0, 0.3)
 
+// Анимация падающих денег
 const moneyTextures = ['/money1.png', '/money2.png', '/money3.png']
 const particles = []
 
@@ -121,6 +130,7 @@ function animateParticles() {
   })
 }
 
+// Основной цикл анимации
 function animate() {
   requestAnimationFrame(animate)
   pyramid.rotation.y += 0.005
@@ -130,6 +140,7 @@ function animate() {
 }
 animate()
 
+// Адаптация под изменение размеров экрана
 window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
